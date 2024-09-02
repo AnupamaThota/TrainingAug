99267c500ba2305ff72e9f9b924450d55ccd98fd
AnupamaThota <anupamathota770@gmail.com>
2024-09-02 12:12:06 +0000

    hello.c

---
+++ commands.sh
@@ -0,0 +1,7 @@
+git add .
+sleep 2
+git command -m"update"
+sleep 2
+git push origin main
+echo files pushed to git
+
---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("Hello\n");
+	printf("My name is Anupama\n");
+	printf("Iam from Tirupati");
+	return 0;
+}
+
